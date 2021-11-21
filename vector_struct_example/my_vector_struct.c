cf47d628f2a54bd34b45cf83d4560e83f87483ee
david <david.mis@rice.edu>
2021-11-20 23:17:29 -0600

    Completed assignment

--- vector_struct_example/my_vector_struct.c
+++ vector_struct_example/my_vector_struct.c
@@ -1,6 +1,7 @@
 #include "my_vector_struct.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 // implementation of the functions
 
@@ -31,9 +32,24 @@ my_vector vector_constructor(const int length){
   }
 }
 
+// Constructor for creating an uninitialized vector.
+// Sets the length field but does not attempt to malloc
+// any memory. Can be used to avoid unnecessary mallocs 
+// whenever an operation that must return a vector errors
+// out.
+my_vector uninitialized_vector(const int length){
+  my_vector vec;
+  vec.v = NULL;
+  vec.length = length;
+  vec.status = 1;
+  return vec;
+}
+
+
 // destructor
 void vector_destructor(my_vector* vec){
   free(vec->v);
+  vec->v = NULL; // Prevent anyone from trying to access this anymore. Also prevents multiple calls to `free`.
   // status is nonzero indicating the vector cannot be used.
   vec->status = 1;
 }
@@ -75,28 +91,112 @@ double vector_get(my_vector* vec, const int index){
 
 // vector scalar multiplication
 my_vector vector_scalar_multiplication(my_vector* v, double num){
-  // initialized a new vector called w
-  my_vector w = vector_constructor(v->length);
+  // Use a length variable so it is obvious w and v have the same   // length.
+  int length = v->length;
 
   // here the input is the scalar num and we have checked that v is a usable vector
-  if (v->status == 0){
-
-    //get the address of w for the pointer
-    my_vector* ptr_w = &w;
-    
-    // for loop for scalar multiplication
-    // continue ... [use vector_set() here]
-    }
-
+  if (v->status != 0) {
+    printf("vector_scalar_multiplication: this is an invalid vector we have inputted"); 
+    return uninitialized_vector(length);
   }
-  else{
-    printf("vector_scalar_multiplication: this is an invalid vector we have inputted");
+  
+  // Check for NaN. Something went wrong if we are multiplying 
+  // by a NaN, so it's best to catch it early.
+  if (isnan(num)) {
+    printf("vector_scalar_multiplication: attempted to multiply by NaN");
+    return uninitialized_vector(length);
+  }
+
+  // initialized a new vector called w
+  my_vector w = vector_constructor(length);
+
+  // Verify we are able to initialize w
+  if (w.status != 0) {
+    printf("vector_scalar_multiplication: unable to initialize new vector");
+    return w;
+  }
+
+  // Set the values in a loop.
+  for (int i = 0; i < length; i++) {
+   vector_set(&w, i, vector_get(v, i) * num); 
   }
 
   return w;
 }
 
-// continue ...
-// implement vector_addition and vector_dot_product
 
+// Vector addition.
+my_vector vector_addition(my_vector* v, my_vector* u) {
+  
+  // Verify u and v have the same length
+  if (v->length != u->length) {
+    printf("vector_addition: Cannot add vectors with different lengths.");
+  } 
+ 
+  // Use a length variable so it is obvious the vectors have 
+  // the same length.
+  int length = v->length;
+
+  // Verify input vectors are valid.
+  if (v->status != 0 || u->status != 0) {
+    printf("vector_addition: this is an invalid vector we have inputted"); 
+    return uninitialized_vector(length);
+  }
+  
+  // initialized a new vector called w
+  my_vector w = vector_constructor(length);
+
+  // Verify we are able to initialize w
+  if (w.status != 0) {
+    printf("vector_addition: unable to initialize new vector");
+    return w;
+  }
+
+  // Set the values in a loop.
+  for (int i = 0; i < length; i++) {
+   vector_set(&w, i, vector_get(v, i) + vector_get(u, i)); 
+  }
+
+  return w;
+}
+
+
+// Vector dot product
+double vector_dot_product(my_vector* v, my_vector* u) {
+  
+  // Verify u and v have the same length
+  if (v->length != u->length) {
+    printf("vector_dot_product: Cannot add vectors with different lengths.");
+  } 
+ 
+  // Use a length variable so it is obvious the vectors have 
+  // the same length.
+  int length = v->length;
+
+  // Verify input vectors are valid.
+  if (v->status != 0 || u->status != 0) {
+    printf("vector_dot_product: this is an invalid vector we have inputted"); 
+    return 0.0;
+  }
+  
+  double tol = 0.0;
+  
+  // Calculate dot product.
+  for (int i = 0; i < length; i++) {
+   tol += vector_get(v, i) * vector_get(u, i); 
+  }
+
+  return tol;
+}
+
+void vector_pretty_print(my_vector* v) {
+	printf("{");
+	if (v->length > 0) {
+		printf("%f", vector_get(v, 0));	
+	}
+	for (int i = 1; i < v->length; i++) {
+		printf(", %f", vector_get(v, i));
+	}
+	printf("}");
+}
 
