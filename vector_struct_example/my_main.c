604b9d27eb82cc680db77a8e8132ab5e23f9659e
AraminaLiu <xl106@rice.edu>
2021-11-22 21:14:12 -0500

    Aramina Liu HW5

--- vector_struct_example/my_main.c
+++ vector_struct_example/my_main.c
@@ -1,19 +1,48 @@
 #include "my_vector_struct.h"
 #include <stdio.h>
 
-int main(){
+int main() {
 
-  // lets start making vector v
-  const int len = 4;
-  my_vector v = vector_constructor(len);
-  my_vector* ptr_v = &v;
+	// lets start making vector v
+	const int len = 4;
+	my_vector v = vector_constructor(len);
+	my_vector* ptr_v = &v;
 
-  // this is the vector v
-  int v_array[4] = {-1,5,2,3};
-  // putting in vector v into the structure of my_vector using vector_set
-  for (int i=0; i < sizeof(v_array)/sizeof(int) ;++i){
-    vector_set(ptr_v,i,v_array[i]);
-  }
-  // continue ...
+	// this is the vector v
+	int v_array[4] = { -1,5,2,3 };
+	// putting in vector v into the structure of my_vector using vector_set
+	for (int i = 0; i < sizeof(v_array) / sizeof(int);++i) {
+		vector_set(ptr_v, i, v_array[i]);
+	}
+	// continue ...
+	my_vector w = vector_constructor(len);
+	my_vector* ptr_w = &w;
+
+	// this is the vector v
+	int w_array[4] = {1,-1,1,-2 };
+	// putting in vector v into the structure of my_vector using vector_set
+	for (int i = 0; i < sizeof(w_array) / sizeof(int);++i) {
+		vector_set(ptr_w, i, w_array[i]);
+	}
+
+	double product;
+	product = vector_dot_product(ptr_v, ptr_w);
+	printf("v*w equals to: %g\n", product);
+
+	my_vector minus;
+	my_vector neg_w = vector_scalar_multiplication(ptr_w, -1.0);
+	my_vector* ptr_neg_w = &neg_w;
+	minus = vector_addition(ptr_v, ptr_neg_w);
+	printf("v - w equals to: ");
+	for (int i = 0; i < minus.length; i++) {
+		printf("%g ", vector_get(&minus, i));
+		
+	}
+	printf("\n");
+
+	return 0;
+
+
+
 
 }
--- vector_struct_example/my_vector_struct.c
+++ vector_struct_example/my_vector_struct.c
@@ -5,92 +5,96 @@
 // implementation of the functions
 
 // constructor
-my_vector vector_constructor(const int length){
+my_vector vector_constructor(const int length) {
 
-  // declaring a vector
-  my_vector vec;
-  vec.v = NULL;
-  
-  // return if a bad length value is given
-  if (length <= 0){
-    printf("vector_constructor: the vector length is negative!\n");
-    vec.length = length;
-    vec.status = 1;
-    return vec;
-  }
-  else{ // length > 0 and we try to allocate memory
-    vec.length = length;
-    vec.v = (double*) calloc(length, sizeof(double));
-    if(vec.v != NULL){ //A == NULL means out of memory
-      vec.status = 0;
-    }
-    else{
-      vec.status = 1;
-    }
-    return vec;
-  }
+	// declaring a vector
+	my_vector vec;
+	vec.v = NULL;
+
+	// return if a bad length value is given
+	if (length <= 0) {
+		printf("vector_constructor: the vector length is negative!\n");
+		vec.length = length;
+		vec.status = 1;
+		return vec;
+	}
+	else { // length > 0 and we try to allocate memory
+		vec.length = length;
+		vec.v = (double*)calloc(length, sizeof(double));
+		if (vec.v != NULL) { //A == NULL means out of memory
+			vec.status = 0;
+		}
+		else {
+			vec.status = 1;
+		}
+		return vec;
+	}
 }
 
 // destructor
-void vector_destructor(my_vector* vec){
-  free(vec->v);
-  // status is nonzero indicating the vector cannot be used.
-  vec->status = 1;
+void vector_destructor(my_vector* vec) {
+	free(vec->v);
+	// status is nonzero indicating the vector cannot be used.
+	vec->status = 1;
 }
 
 // set <-- this function writes to a value in a given index, in the vector
-void vector_set(my_vector* vec, const int index, const double value){
-  if (vec->status == 0){
-    if (index > vec->length - 1){
-      printf("vector_set: index exceeds vector length\n");
-    }
-    else{
-      // set value.
-      vec->v[index] = value;
-    }
-  }
-  else {
-    printf("vector_set: this vector cannot be used now\n");
-  }
+void vector_set(my_vector* vec, const int index, const double value) {
+	if (vec->status == 0) {
+		if (index > vec->length - 1) {
+			printf("vector_set: index exceeds vector length\n");
+		}
+		else {
+			// set value.
+			vec->v[index] = value;
+		}
+	}
+	else {
+		printf("vector_set: this vector cannot be used now\n");
+	}
 }
 
 // get <-- this function reads the a value from a given index, from the vector
-double vector_get(my_vector* vec, const int index){
-  double value = 0.0;
-  if (vec->status == 0){
-    if (index > vec->length - 1){
-      printf("vector_get: index exceeds vector length\n");
-    }
-    else{
-      // set value.
-      value = vec->v[index];
-    }
-  }
-  else {
-    printf("vector_get: this vector cannot be used now\n");
-  }
-  return value;
+double vector_get(my_vector* vec, const int index) {
+	double value = 0.0;
+	if (vec->status == 0) {
+		if (index > vec->length - 1) {
+			printf("vector_get: index exceeds vector length\n");
+		}
+		else {
+			// set value.
+			value = vec->v[index];
+		}
+	}
+	else {
+		printf("vector_get: this vector cannot be used now\n");
+	}
+	return value;
 }
 
 
 // vector scalar multiplication
-my_vector vector_scalar_multiplication(my_vector* v, double num){
-  // initialized a new vector called w
-  my_vector w = vector_constructor(v->length);
+my_vector vector_scalar_multiplication(my_vector* v, double num) {
+	// initialized a new vector called w
+	my_vector w = vector_constructor(v->length);
 
-  // here the input is the scalar num and we have checked that v is a usable vector
-  if (v->status == 0){
+	// here the input is the scalar num and we have checked that v is a usable vector
+	if (v->status == 0) {
 
-    //get the address of w for the pointer
-    my_vector* ptr_w = &w;
-    
-    // for loop for scalar multiplication
-    // continue ... [use vector_set() here]
-    }
+		//get the address of w for the pointer
+		my_vector* ptr_w = &w;
 
-  }
-  else{
-    printf("vector_scalar_multiplication: this is an invalid vector we have inputted");
+		// for loop for scalar multiplication
+		for (int i = 0; i < v->length; i++) {
+			vector_set(ptr_w, i, num*vector_get(v, i));
+		}
+
+		// continue ... [use vector_set() here]
+	}
+
+
+  else {
+	  printf("vector_scalar_multiplication: this is an invalid vector we have inputted");
   }
 
   return w;
@@ -98,5 +102,69 @@ my_vector vector_scalar_multiplication(my_vector* v, double num){
 
 // continue ...
 // implement vector_addition and vector_dot_product
+my_vector vector_addition(my_vector* v1, my_vector* v2) {
+	// initialized a new vector called w
+	my_vector w = vector_constructor(v1->length);
+
+	if (v1->status != 0) {
+		printf("vector_addition: the first vector is an invalid vector we have inputted");
+	}
+
+	else if (v2->status != 0) {
+		printf("vector_addition: the second vector is an invalid vector we have inputted");
+	}
+	// here the input is the scalar num and we have checked that v is a usable vector
+	else if (v1->length != v2->length) {
+		printf("vector_addition: two vectors are of different length");
+		
+	}
 
 
+	else {
+		//get the address of w for the pointer
+		my_vector* ptr_w = &w;
+
+		// for loop for scalar multiplication
+		for (int i = 0; i < v1->length; i++) {
+			vector_set(ptr_w, i, vector_get(v1, i) + vector_get(v2, i));
+		}
+
+		// continue ... [use vector_set() here]
+		
+	}
+
+	return w;
+}
+
+double vector_dot_product(my_vector* v1, my_vector* v2) {
+	// initialized a new vector called w
+	double res;
+
+	if (v1->status != 0) {
+		printf("vector_dot_product: the first vector is an invalid vector we have inputted");
+	}
+
+	else if (v2->status != 0) {
+		printf("vector_dot_product: the second vector is an invalid vector we have inputted");
+	}
+	// here the input is the scalar num and we have checked that v is a usable vector
+	else if (v1->length != v2->length) {
+		printf("vector_dot_product: two vectors are of different length");
+
+	}
+
+
+	else {
+		//get the address of w for the pointer
+		res = 0;
+
+		// for loop for scalar multiplication
+		for (int i = 0; i < v1->length; i++) {
+			res +=  vector_get(v1, i) * vector_get(v2, i);
+		}
+
+		// continue ... [use vector_set() here]
+	}
+
+	return res;
+}
